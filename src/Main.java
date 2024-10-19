117a0020ce195b7dea72f99057995a3158049cbc
Геннадий <cherri.irk@yandex.ru>
2024-10-19 21:45:44 +0800

    new Hw10

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,24 @@ public class Main {
     public static void main(String[] args) {
         // Task 1
         System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        {
+            System.out.println("Ф. И. О. сотрудника — " + fullName);
+        }
+        // Task 2
+        System.out.println("Задача 2");
+        {
+            System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета —  "+fullName.toUpperCase());
+        }
+        // Task 3
+        System.out.println("Задача 3");
+        String fullNameThree = "Иванов Семён Семёнович";
+        String s2 = fullNameThree.replace('ё', 'е');
+        {
+            System.out.println("Данные Ф. И. О. сотрудника — "+s2);
+        }
     }
 }
